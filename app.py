fa0927897876ee5f5767bd218209c0a44fda1d94
wilson7777777 <wilsononline2@yahoo.com>
2025-09-06 15:56:56 +0100

    feat: Add new /about route

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello again, CI/CD!'
+
+@app.route('/about')
+def about():
+    return 'This is a simple Flask application deployed with a CI/CD pipeline.'
