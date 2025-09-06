486e1fa220494c6c5f78e8df900a39d728f2c832
wilson7777777 <wilsononline2@yahoo.com>
2025-09-06 15:46:18 +0100

    Fix assertion error in test

--- app.py
+++ app.py
@@ -3,8 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, CI/CD!</h1>'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello():
+    return 'Hello again, CI/CD!'
